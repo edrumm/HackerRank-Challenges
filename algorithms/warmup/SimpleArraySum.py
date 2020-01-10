6876e9705f1a642495c1508ad56b4363df8bf818
edrumm <esd0658@googlemail.com>
2020-01-10 00:18:21 +0000

    first commit

---
+++ algorithms/warmup/SimpleArraySum.py
@@ -0,0 +1,9 @@
+def simple_array_sum(arr):
+    sum = 0
+    for value in arr:
+        sum += value
+
+    return sum
+
+
+print(simple_array_sum([1, 2, 3, 4, 10, 11]))
---
+++ algorithms/warmup/SolveMeFirst.py
@@ -0,0 +1,7 @@
+def solve_me_first(a, b):
+    return a + b
+
+
+a = input("A")
+b = input("B")
+print(solve_me_first(a, b))
