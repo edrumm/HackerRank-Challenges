ba684e920beccc758eb759a7a4e92f2cec8f21de
edrumm <esd0658@googlemail.com>
2020-01-16 20:22:11 +0000

    implemented PlusMinus

---
+++ algorithms/warmup/PlusMinus.py
@@ -0,0 +1,20 @@
+def plus_minus(arr):
+    if not arr: return
+
+    pos, neg, zero = 0, 0, 0
+    l = len(arr)
+
+    for num in arr:
+        if not num:
+            zero += 1
+        elif num > 0:
+            pos += 1
+        else:
+            neg += 1
+
+    print(pos / l)
+    print(neg / l)
+    print(zero / l)
+
+
+print(plus_minus([1, 1, 0, -1, -1]))
