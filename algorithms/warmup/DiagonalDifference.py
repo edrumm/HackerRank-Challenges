36dbb49721670769c147ef436e5b4f9ba54a96bb
edrumm <esd0658@googlemail.com>
2020-01-16 19:45:48 +0000

    implemented DiagonalDifference

--- algorithms/warmup/DiagonalDifference.py
+++ algorithms/warmup/DiagonalDifference.py
@@ -1,5 +1,13 @@
 def diagonal_difference(arr):
+    primary = 0
+    secondary = 0
 
     for i in range(len(arr)):
-        # TODO
-        pass
+        j = len(arr[0]) - i - 1
+        primary += arr[i][i]
+        secondary += arr[i][j]
+
+    return abs(primary - secondary)
+
+
+print(diagonal_difference([[11, 2, 4], [4, 5, 6], [10, 8, -12]]))
